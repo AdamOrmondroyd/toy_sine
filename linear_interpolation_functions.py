0f3ad686d40a5e752670ef6d7d598271d7f16a92
Ormorod <Adam.Ormondroyd@gmail.com>
2021-11-16 12:44:23 +0000

    Added scipy end nodes method, and took the modulo of the input x for my own cyclic method

--- linear_interpolation_functions.py
+++ linear_interpolation_functions.py
@@ -13,15 +13,35 @@ from numpy import (
     less_equal,
     outer,
 )
+from scipy.interpolate import interp1d
 
 
 def f_end_nodes_numpy(x, params):
+    """
+    Vectoried linear interpolstion function using end nodes.
+
+    params = [x1, x2, xn, ..., y0, y1, y2, ..., yn, yn+1]
+    """
     n = len(params) // 2 - 1
     return interp(
         x,
         concatenate(([0], params[:n], [wavelength])),
         params[n:],
     )
+
+
+def f_end_nodes_scipy(x, params):
+    """
+    Vectorized linear interpolstion function using end nodes implemented with scipy.interpolate.interp1d
+
+    params = [x1, x2, ..., xn, y0, y1, y2, ..., yn, yn+1]
+    """
+    n = len(params) // 2 - 1
+    return interp1d(
+        concatenate(([0], params[:n], [wavelength])),
+        params[n:],
+        fill_value="interpolate",
+    )(x)
 
 
 def f_cyclic_numpy(x, params):
@@ -37,7 +57,7 @@ def f_cyclic_numpy(x, params):
 
 def f_cyclic_adam(x, params):
     """Vectorised cyclic linear interpolation function done manually."""
-    x %= wavelength
+    x_mod_wavelength = x % wavelength
     n = len(params) // 2
     xp = params[:n]
     yp = params[n:]
@@ -47,7 +67,10 @@ def f_cyclic_adam(x, params):
     cs = yp[:-1] - ms * xp[:-1]
 
     return (outer(x, ms) + cs)[
-        logical_and(less_equal.outer(xp[:-1], x), greater.outer(xp[1:], x)).T
+        logical_and(
+            less_equal.outer(xp[:-1], x_mod_wavelength),
+            greater.outer(xp[1:], x_mod_wavelength),
+        ).T
     ]
 
 
