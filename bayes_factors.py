9e47d3077c23a5169f00a79598c3cb91229ea219
Ormorod <Adam.Ormondroyd@gmail.com>
2021-12-01 14:10:51 +0000

    tidied up bayes factor plots

--- bayes_factors.py
+++ bayes_factors.py
@@ -31,10 +31,16 @@ def plot_bayes_factors(
     # else:
     subscripts = [f"{{{i},{j}}}" for (i, j) in pairs]
     labels = [r"$B_{}$".format(subscript) for subscript in subscripts]
-    ax.bar(np.arange(len(Bs)), Bs, yerr=sigma_Bs)
+    ax.bar(
+        np.arange(len(Bs)),
+        Bs,
+        yerr=sigma_Bs,
+        color="c",
+        error_kw={"capsize": 1.0, "elinewidth": 0.5, "capthick": 0.5},
+    )
     ax.set_xticks(np.arange(len(Bs)))
     ax.set_xticklabels(labels)
-    ax.set_ylim(-10, 2)
+    ax.set_ylim(bottom=-10)
 
     plt.savefig(f"plots/{line_or_sine}{'_x_errors' if x_errors else ''}_bayes.png")
 
